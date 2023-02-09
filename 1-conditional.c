64c66774dea1178f0c22b9334db959f3dd321468
Zach Riane Machacon <zachrianemachacon@yahoo.com>
2023-02-10 02:47:45 +0800

    initial commit

---
+++ 1-conditional.c
@@ -0,0 +1,12 @@
+/* Write a program that asks for the user's name via input() function. 
+If the name is "Bond" make it print "Welcome on board 007." 
+If the name is "Ian" make it print "Yo!"
+Otherwise make it print "Good morning {NAME}". (Replace Name with user's name) */
+
+#include <stdio.h>
+
+int main() {
+
+    
+    return 0;
+}
